b863c949d7af687c617c149e98f4aa6b82a0beee
ValentynVarenyk <varenykvalentyn@gmail.com>
2025-08-20 12:52:06 +0300

    95% hw-26 completed

--- js/index.js
+++ js/index.js
@@ -2,11 +2,13 @@
   const refs = {
     openModalBtn: document.querySelector("[data-modal-open]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
+    closeModalBtn1: document.querySelector("[data-modal-close1]"),
     modal: document.querySelector("[data-modal]"),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn1.addEventListener("click", toggleModal);
 
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
